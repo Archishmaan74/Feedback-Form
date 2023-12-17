7a3c030c39a9e1493e5b690da6a8d0379ecaa552
Archishmaan <archishmaan.upa4@gmail.com>
2023-12-17 16:24:04 +0530

    new components & routing commit

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { ReviewsComponent } from './reviews/reviews.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "", component: FeedbackComponent
+  },
+  {
+    path: "feedback", component: FeedbackComponent
+  },
+  {
+    path: "reviews", component: ReviewsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { ReviewsComponent } from './reviews/reviews.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FeedbackComponent,
+    ReviewsComponent
   ],
   imports: [
     BrowserModule,
